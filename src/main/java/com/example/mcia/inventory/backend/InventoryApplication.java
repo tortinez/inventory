377c674e61485ea236072975d680d4781dc39a93
tortinez <victormavi_1995@hotmail.com>
2018-10-01 10:38:40 +0200

    Import project generated using start.spring.io

---
+++ src/main/java/com/example/mcia/inventory/backend/InventoryApplication.java
@@ -0,0 +1,12 @@
+package com.example.mcia.inventory.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryApplication.class, args);
+	}
+}
