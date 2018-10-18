bf360220ce9db0d33b76bdf0b33c8ee4c6dd0f53
tortinez <victormavi_1995@hotmail.com>
2018-10-18 13:10:50 +0200

    Change Id by an optional argument

--- src/main/java/com/mcia/inventory/backend/service/converter/RequestConverter.java
+++ src/main/java/com/mcia/inventory/backend/service/converter/RequestConverter.java
@@ -1,6 +1,6 @@
 package com.mcia.inventory.backend.service.converter;
 
 public interface RequestConverter<T,R> {
-    T toEntity(R request, Long id);
+    T toEntity(R request, Long... optId);
 
 }
